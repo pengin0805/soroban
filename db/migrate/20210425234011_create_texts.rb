e9d7d38b0ba4f969e19b58dae078eafaa6b4681d
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-26 09:12:57 +0900

    db:migrate実行

--- db/migrate/20210425234011_create_texts.rb
+++ db/migrate/20210425234011_create_texts.rb
@@ -1,7 +1,9 @@
 class CreateTexts < ActiveRecord::Migration[6.0]
   def change
     create_table :texts do |t|
-
+      t.string      :text_name,      null: false
+      t.text        :text_info,      null: false
+      t.string      :text_class
       t.timestamps
     end
   end
