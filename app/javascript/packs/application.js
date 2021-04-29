2d46218d9821772d1856882d3a8bb2eaded67bce
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-29 19:26:40 +0900

    ファイル名修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../times_table")
+require("../time_table")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
