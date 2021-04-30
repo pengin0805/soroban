fa736f064142c1cdc8bee973dc85c057dd946542
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-30 12:36:01 +0900

    （カレンダー）indexアクションにインスタンス変数追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,5 +1,9 @@
 class TextsController < ApplicationController
   def index
+    @today = Date.today
+    from_date = Date.new(@today.year, @today.month, @today.beginning_of_month.day).beginning_of_week(:sunday)
+    to_date = Date.new(@today.year, @today.month, @today.end_of_month.day)
+    @calendar_data = from_date.upto(to_date)
   end
 
   def study
