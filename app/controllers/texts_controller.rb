fa402b5d3e1601be760220350b813241d3fbccc3
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-01 11:40:34 +0900

    （カレンダー）Dateオブジェクトを用いたコントローラー追記

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -7,6 +7,10 @@ class TextsController < ApplicationController
   end
 
   def study
+    @today = Date.today
+    from_date = Date.new(@today.year, @today.month, @today.beginning_of_month.day).beginning_of_week(:sunday)
+    to_date = Date.new(@today.year, @today.month, @today.end_of_month.day)
+    @calendar_data = from_date.upto(to_date)
     @time_table = Multiplication.order("RAND()").limit(3)
     render partial: "studies/#{params[:name]+params[:id]}" and return
   end
