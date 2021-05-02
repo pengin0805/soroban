913d80a582ac2a3fb5867e9668bbc5014cfbf107
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-03 07:01:31 +0900

    Update arry?.js

--- app/javascript/arry?.js
+++ app/javascript/arry?.js
@@ -1,25 +1,28 @@
-
-window.addEventListener('load',function(){
+if(localStorage.getItem("date")){
   const history = [];
-  if(localStorage.getItem("date")){
-    history = JSON.parse(localStorage.getItem("data"));
+  JSON.parse(localSrorage.getItem("date"));
+  history.push("date");
   }
-    // const before = localStorage.getItem("date");
-  history.forEach(today => {
-    const before = document.getElementById(today+"");
-    before.setAttribute("bgcolor","red");
+  
+  window.addEventListener('load',function(){
+    if(localStorage.getItem("date")){
+    const before = localStorage.getItem("date");
+    const before_set = document.getElementById(before+"");
+    before_set.setAttribute("bgcolor","red");
+    }
+  
+    const today = new Date();
+    const date = today.getDate();
+    const month = today.getMonth()+1;
+    const year = today.getFullYear();
+    const set = document.getElementById(year+"-"+("0"+month).slice(-2)+"-"+("0"+date).slice(-2));
+    const history = [];
+    history.push(year+"-"+("0"+month).slice(-2)+"-"+("0"+date).slice(-2));
+  
+    const button = document.getElementById("result");
+  
+    button.addEventListener("click",() => {
+    const color = set.setAttribute("bgcolor","red");
+    localStorage.setItem("date",JSON.stringify(history));
+    })
   })
-
-  const today = new Date();
-  const date = today.getDate();
-  const month = today.getMonth()+1;
-  const year = today.getFullYear();
-  const set = document.getElementById(year+"-"+("0"+month).slice(-2)+"-"+("0"+date).slice(-2));
-
-  const button = document.getElementById("result");
-  button.addEventListener("click",() => {
-  const color = set.setAttribute("bgcolor","red");
-  history.push(year+"-"+("0"+month).slice(-2)+"-"+("0"+date).slice(-2));
-  localStorage.setItem("date",JSON.stringify(history));
-  });
-})
