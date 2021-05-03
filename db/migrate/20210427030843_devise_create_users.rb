bbf404211e6551f0faa42bb1e735d12246351152
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-03 18:18:28 +0900

    （クレカ）cardsテーブル作成

--- db/migrate/20210427030843_devise_create_users.rb
+++ db/migrate/20210427030843_devise_create_users.rb
@@ -12,6 +12,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :subscription_id
+      t.boolean :premium, defaule: false, null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
