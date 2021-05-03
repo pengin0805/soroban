ddae809dcade4f27ae941dd1a0edb19b87b71119
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-03 18:18:48 +0900

    （クレカ）ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'cards/new'
   devise_for :users
   root to: 'texts#index'
   get 'texts/:name/:id', controller: 'texts', action: 'study'
   get 'texts/search'
+  resources :cards, only: [:new, :create]
+  resources :users, only: [:show, :update]
 end
