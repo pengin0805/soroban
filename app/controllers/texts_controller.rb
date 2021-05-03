b064b75a1d7d84777684f681ed604863d6ed4b0d
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-03 12:28:58 +0900

    （検索）searchアクションの追加

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,9 @@
 class TextsController < ApplicationController
+
+  before_action :search_product, only: [:index, :search]
+
   def index
-    @text = Text.all
+    @texts = Text.all
     @today = Date.today
     from_date = Date.new(@today.year, @today.month, @today.beginning_of_month.day).beginning_of_week(:sunday)
     to_date = Date.new(@today.year, @today.month, @today.end_of_month.day)
@@ -13,7 +16,17 @@ class TextsController < ApplicationController
     to_date = Date.new(@today.year, @today.month, @today.end_of_month.day)
     @calendar_data = from_date.upto(to_date)
     @time_table = Multiplication.order("RAND()").limit(3)
-    render partial: "studies/#{params[:name]+params[:id]}" and return
+    render partial: "studies/#{params[:name]+params[:id]}" 
+  end
+
+  def search
+    @results = @p.result.all
+  end
+
+  private
+
+  def search_product
+    @p = Text.ransack(params[:q])
   end
   
 end
