81e31d2f5f8f4e6a5b0263494955cf9d4d823cdc
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-04 16:32:16 +0900

    Capistranoの導入（自動デプロイ）

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+lock 'capistrano (3.16.0)'
+set :application, 'soroban'
+set :repo_url,  'git@github.com:pengin0805/soroban.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/mykey.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
