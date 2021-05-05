56b58106c7fe6021a5992173d8646ab4561cbcb5
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-05 18:44:44 +0900

    （定期購入）usesテーブルにカラム追加
    subscription_id , premium

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_05_03_055434) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "subscription_id"
+    t.boolean "premium", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
